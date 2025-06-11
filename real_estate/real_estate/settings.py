c5a1f1b0832071ae662c585c8562c00e43730b2d
Anjali <anjaliajayan95@gmail.com>
2025-06-11 12:32:33 +0530

    Update settings

--- real_estate/real_estate/settings.py
+++ real_estate/real_estate/settings.py
@@ -124,7 +124,7 @@ SOCIALACCOUNT_PROVIDERS = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = config('EMAIL_HOST')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
